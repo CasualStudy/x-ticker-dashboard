bf92c30de1f67f43a33ba337ed522d0b3c22f18f
Dongzhe Wu <dongzhewu@Dongzhes-MacBook-Pro-2.local>
2026-06-05 20:36:48 +0800

    Add exchange distribution data and pie chart

---
+++ exchange_data.js
@@ -0,0 +1,14 @@
+const exchangeData = {
+  "NYSE": 79,
+  "NGM": 18,
+  "Unknown": 33,
+  "NASDAQ": 109,
+  "PCX": 6,
+  "NCM": 36,
+  "OTC Markets": 16,
+  "OQX": 3,
+  "ASE": 4,
+  "YHD": 11,
+  "BTS": 1,
+  "OID": 1
+};
